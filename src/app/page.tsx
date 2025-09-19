915523ae7b87baa5d23d3a41770be3350e2ad77a
ShahulHameedBZR <shahul200627@gmail.com>
2025-09-19 20:06:40 +0800

    SCRUM-24 Establish Core API Layer with tRPC
    SCRUM-24 CHORE Establish Core API Layer with tRPC
    
    - install dependencies for tRPC packages
    - set up init tRPC
    - set up root App Router exposed via Nest.js API route with a sample route to test/refer. (server-side tRPC router)
    - set up tRPC client and React Query provider on front end
    
    As of now, tRPC is successful when using local dummy data. tRPC call is successful and data is shown on home page.
    
    Will commit here to safe and udpate later with tRPC context when prisma is set up.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+"use client";
+
+import { trpc } from "./lib/trpc"; // use client util
+
 export default function Home() {
+  const getTrpcSample = trpc.trpcRouteSample.getTrpcSampleUsers.useQuery();
   return (
     <main>
       <h1>All-in-One Project</h1>
       <p>Next.js application with TypeScript, ESLint, and App Router.</p>
       <p>Ready for development!</p>
+
+      <h3>{JSON.stringify(getTrpcSample.data)}</h3>
     </main>
   )
 }
---
+++ src/app/server/routers/_app.ts
@@ -0,0 +1,20 @@
+// this is our app router
+
+// routes are imported here from server/router/ .ts
+import { trpcRouteSampleRouter } from './trpcRouteSample';
+
+// here we import from our init (server/trpc.ts)
+import { router } from '../trpc';
+
+// remember to export so that it is callable in api/trpc/[trpc]/routes.ts
+export const appRouter = router({
+    // ... here is where our routes are defined
+
+    // name it so that it is easier and neater to call. Basically without router
+    trpcRouteSample: trpcRouteSampleRouter,
+
+});
+
+// Export type router type signature,
+// NOT the router itself.
+export type AppRouter = typeof appRouter;
