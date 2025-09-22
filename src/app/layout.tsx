90a3bc1a1a3182a5147804c532109337378db6a8
Tan Zhong Yan <123535338+tanzhongyan@users.noreply.github.com>
2025-09-22 22:32:41 +0800

    Fix(db) prisma path and persistent storage (#11)
    * fix: husky windows compatibility
    
    -Make husky compatible for windows by adding the #!/usr/bin/env bash shebang to .husky/pre-commit as well as wsl installation process
    -Remove duplicated packages from package.json
    
    * Fix(db): ephemeral/shortlived storage
    
    #9
    - Removed persistent volume to allow for more flexible development
    - Tested to with `cd supabase`,  `docker compose --env-file ../.env down -v --remove-orphans`, and `docker compose --env-file ../.env up -d` which showed no data persisting
    - Removed outdated readme.md regarding husky windows compatibility issues
    
    * Fix: prisma path
    
    - Use standard default path instead of custom path
    
    * test: checking pre-commit hooks
    
    * Fix: eslint ignores
    
    - use default prisma path instead of custom path
    - tested precommit hooks for eslint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,18 @@
-import type { Metadata } from 'next'
+import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'All-in-One Project',
   description: 'Team project built with Next.js',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body>{children}</body>
     </html>
-  )
+  );
 }
