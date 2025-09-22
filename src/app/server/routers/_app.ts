6e86c542d9c6622e377fe4bd405067cf0cc53361
ShahulHameedBZR <shahul200627@gmail.com>
2025-09-22 12:21:03 +0800

    SCRUM-24 Establish Core API Layer with tRPC
    SCRUM-24 CHORE Establish Core API Layer with tRPC
    
    - created context for prisma client connection
    - created mock front end to showcase examples and also verify integration of tRPC, Prisma, DB, and frontend.
    - mock frontend also used to verify basic CRUD database operations are possible#

--- src/app/server/routers/_app.ts
+++ src/app/server/routers/_app.ts
@@ -3,16 +3,18 @@
 // routes are imported here from server/router/ .ts
 import { trpcRouteSampleRouter } from './trpcRouteSample';
 
+import { commentsRouter } from './comment';
+
 // here we import from our init (server/trpc.ts)
 import { router } from '../trpc';
 
 // remember to export so that it is callable in api/trpc/[trpc]/routes.ts
 export const appRouter = router({
-    // ... here is where our routes are defined
+  // ... here is where our routes are defined
 
-    // name it so that it is easier and neater to call. Basically without router
-    trpcRouteSample: trpcRouteSampleRouter,
-
+  // name it so that it is easier and neater to call. Basically without router
+  trpcRouteSample: trpcRouteSampleRouter,
+  comments: commentsRouter, // placeholder for test
 });
 
 // Export type router type signature,
