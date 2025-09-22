931fcb571b3d9b428444aaa2be958216a13c275d
ShahulHameedBZR <shahul200627@gmail.com>
2025-09-22 23:39:30 +0800

    SCRUM-24 dummy clean up
    Clean up dummy used in staging. Note testing here was done this way given what is already installed (jest not done yet).
    
    - clean up routes under src\app\server\router (trpcRouteSample.ts and comment.ts)
    - ckean up AppRouter to empty in src\app\server\router\_app.ts
    - reset page.tsx to default provided

--- src/app/server/routers/_app.ts
+++ src/app/server/routers/_app.ts
@@ -1,9 +1,6 @@
 // this is our app router
 
 // routes are imported here from server/router/ .ts
-import { trpcRouteSampleRouter } from './trpcRouteSample';
-
-import { commentsRouter } from './comment';
 
 // here we import from our init (server/trpc.ts)
 import { router } from '../trpc';
@@ -11,10 +8,6 @@ import { router } from '../trpc';
 // remember to export so that it is callable in api/trpc/[trpc]/routes.ts
 export const appRouter = router({
   // ... here is where our routes are defined
-
-  // name it so that it is easier and neater to call. Basically without router
-  trpcRouteSample: trpcRouteSampleRouter,
-  comments: commentsRouter, // placeholder for test
 });
 
 // Export type router type signature,
