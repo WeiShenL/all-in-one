12ddc4b5f0b1c62f166273c14366d8d15380c5ab
ShahulHameedBZR <shahul200627@gmail.com>
2025-09-26 21:34:53 +0800

    SCRUM 28 Staging Workflow
    - disable linting for vercel build since we have alrdy done so for pre
      commit and on PR#

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,15 @@ const nextConfig: NextConfig = {
     NEXT_PUBLIC_API_EXTERNAL_URL: process.env.NEXT_PUBLIC_API_EXTERNAL_URL,
     NEXT_PUBLIC_ANON_KEY: process.env.NEXT_PUBLIC_ANON_KEY,
   },
+  // Disable ESLint during builds in production (Vercel)
+  eslint: {
+    ignoreDuringBuilds: process.env.VERCEL === '1',
+  },
+
+  // Disable TypeScript errors during builds in production (Vercel) - use with caution
+  typescript: {
+    ignoreBuildErrors: process.env.VERCEL === '1',
+  },
   /* config options here */
 };
 
